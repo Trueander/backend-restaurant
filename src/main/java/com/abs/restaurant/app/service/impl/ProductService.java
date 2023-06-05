a32c8d2faa923ca45a0fa043d24b0c628947b0fe
Anderson <anderson.bengolea@gmail.com>
2023-06-04 20:47:34 -0500

    fix

--- src/main/java/com/abs/restaurant/app/service/impl/ProductService.java
+++ src/main/java/com/abs/restaurant/app/service/impl/ProductService.java
@@ -171,7 +171,7 @@ public class ProductService implements IProductService {
         product.setPrice(parseFromCellToBigDecimal(dataFormatter.formatCellValue(row.getCell(3))));
         product.setStock(parseFromCellToInteger(dataFormatter.formatCellValue(row.getCell(4))));
         Category category = new Category();
-        category.setId((long) parseFromCellToInteger(dataFormatter.formatCellValue(row.getCell(5))));
+        category.setId(parseFromCellToLong(dataFormatter.formatCellValue(row.getCell(5))));
         product.setCategory(category);
         return product;
     }
@@ -199,12 +199,24 @@ public class ProductService implements IProductService {
     }
 
     private Integer parseFromCellToInteger(String value) {
-        if (value == null) {
+        if (value == null || (value != null && value.trim().isEmpty())) {
             return null;
         }
 
         try {
             return Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+    }
+
+    private Long parseFromCellToLong(String value) {
+        if (value == null || (value != null && value.trim().isEmpty())) {
+            return null;
+        }
+
+        try {
+            return Long.parseLong(value);
         } catch (NumberFormatException e) {
             return null;
         }
